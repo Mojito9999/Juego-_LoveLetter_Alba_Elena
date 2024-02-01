f434cc5ddc8cbc5dea26173143eb5fa90b7abd56
elena <ecanizaresj99@gmail.com>
2024-02-01 13:27:55 +0100

    Commit 1

---
+++ app/src/main/java/com/example/jugeo_loveletter_alba_elena/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.jugeo_loveletter_alba_elena;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Jugeo_LoveLetter_Alba_Elena"
+include(":app")
+ 
