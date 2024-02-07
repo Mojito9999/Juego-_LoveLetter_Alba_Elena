5c3275440f27b155f0b31885b9c7aca07d739d49
elena <ecanizaresj99@gmail.com>
2024-02-07 13:08:18 +0100

    Commit empezar layout

--- app/src/main/java/com/example/jugeo_loveletter_alba_elena/MainActivity.java
+++ app/src/main/java/com/example/jugeo_loveletter_alba_elena/MainActivity.java
@@ -1,17 +1,82 @@
 package com.example.jugeo_loveletter_alba_elena;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityResultLauncher<Intent> launcher;
+    ImageView imagen;
+    private final int REQUEST_PERMISSIONS = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imagen = findViewById(R.id.imageView);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(getApplicationContext(),
+                        android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(getApplicationContext(),
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                        PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    MainActivity.this,
+                    new String[]{
+                            android.Manifest.permission.CAMERA,
+                            android.Manifest.permission.RECORD_AUDIO,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_PERMISSIONS);
+        }
+        launcher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK) {
+                            Intent data = result.getData();
+                            Bundle extras = data.getExtras();
+                            Bitmap imgBitmap = (Bitmap) extras.get("data");
+                            imagen.setImageBitmap(imgBitmap);
+                        }
+                    }
+                }
+        );
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == REQUEST_PERMISSIONS){
+            for(int i=0; i< grantResults.length; i++){
+                if(grantResults[i]== PackageManager.PERMISSION_DENIED){
+                    Toast.makeText(this, "La app no tiene permisos", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }else{
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+    public static class Bot {
     }
 
-    public static class Bot {
+    public void tomarFoto(View view) {
+        Intent tomarFoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        launcher.launch(tomarFoto);
     }
 }
